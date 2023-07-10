e4501b4df3cd5046fbc62e03159d1d36b9e8b865
weeqn <gugobute@gmail.com>
2023-07-10 14:36:25 +0300

    added ExplodeScene

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
--- Assets/Key.cs
+++ Assets/Key.cs
@@ -5,32 +5,67 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRange;
+    [SerializeField] private float _timeToExplode;
+    private float _explosionTimer;
+
     [SerializeField] private GameObject highlight;
-    private KeyCode _keyCode;
+    [SerializeField] private KeyCode m_keyCode;
 
     private void Awake()
     {
-        Enum.TryParse(gameObject.name, true, out _keyCode);
-        if (_keyCode == KeyCode.None)
+        _explosionTimer = _timeToExplode;
+
+        if (m_keyCode == KeyCode.None)
         {
-            throw new Exception("can't identify button: " + gameObject.name);
+            Enum.TryParse(gameObject.name, true, out m_keyCode);
+            if (m_keyCode == KeyCode.None)
+                throw new Exception("can't identify button: " + gameObject.name);
         }
+        
     }
 
-    private void SetHighlight(bool isButtonPressed)
+    private void FillHighlight(bool isButtonPressed)
     {
         highlight.SetActive(isButtonPressed);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(_keyCode))
+        if (Input.GetKey(m_keyCode))
         {
-            SetHighlight(true);
+            _explosionTimer -= Time.deltaTime;
         }
-        if (Input.GetKeyUp(_keyCode))
+        if (Input.GetKeyUp(m_keyCode))
         {
-            SetHighlight(false);
+            _explosionTimer = _timeToExplode;
         }
+
+        if (_explosionTimer <= 0)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _explosionRange);
+        foreach (Collider2D collider in colliders)
+        {
+            Vector2 direction = collider.transform.position - transform.position;
+            Rigidbody2D rigidbody2D = collider.GetComponent<Rigidbody2D>();
+            if (rigidbody2D != null)
+            {
+                rigidbody2D.AddForce(direction * _explosionForce);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _explosionRange);
     }
 }
